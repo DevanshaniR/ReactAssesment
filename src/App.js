9d4a6abdb49ddd38b3629ff17a04932e46df2ea9
Devanshani Rasanjika <devanshani@omobio.net>
2021-05-09 18:48:53 +0530

    create routing for order submit view & verify view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import OrderSubmitView from './views/OrderSubmitView';
+import OrderVerifyView from './views/OrderVerifyView';
+
 
 function App() {
   return (
-    <OrderSubmitView />
+    <Router>
+      <Route path="/orderVerify" exact component={OrderVerifyView} />
+      <Route path="/" exact component={OrderSubmitView} />
+    </Router>
   );
 }
 export default App;
